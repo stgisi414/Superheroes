aacb518a85a4e772324accddbfba2ef159bf331c
Stefan Gisi <141892560+stgisi414@users.noreply.github.com>
2025-06-29 08:23:03 +0000

    blah blah

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,34 @@
-
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-      plugins: [react()],
-      define: {
-        'process.env': env
+  const env = loadEnv(mode, ".", "");
+  return {
+    plugins: [react()],
+    define: {
+      "process.env.GEMINI_API_KEY": JSON.stringify(
+        env.GEMINI_API_KEY || process.env.GEMINI_API_KEY,
+      )
+    },
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      },
-      server: {
-        host: true,
-        port: 5173,
-        allowedHosts: ['dc6fd3fc-1ae9-41e5-b6a6-cdd53582d9eb-00-1akqpj7omovi5.riker.replit.dev', 'superhearoesare.cool', 'superheroez.replit.app']
-      },
-      preview: {
-        host: '0.0.0.0',
-        port: 5173
-      }
-    };
+    },
+    server: {
+      host: true,
+      port: 5173,
+      allowedHosts: [
+        "dc6fd3fc-1ae9-41e5-b6a6-cdd53582d9eb-00-1akqpj7omovi5.riker.replit.dev",
+        "superhearoesare.cool",
+        "superheroez.replit.app",
+      ],
+    },
+    preview: {
+      host: "0.0.0.0",
+      port: 5173,
+    },
+  };
 });
